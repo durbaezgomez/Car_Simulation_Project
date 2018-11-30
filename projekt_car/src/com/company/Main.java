6d71df8ae62f784738c7ebff5417ac8b001cf47a
Dominik <dominiug@yahoo.com>
2018-11-30 17:26:29 +0100

    Initial commit

---
+++ projekt_car/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Car{
+
+    float engineThrotle;
+    int gear;
+    float acceleration;
+    bool breakOn;
+    bool engineOn;
+
+
+
+
+}
